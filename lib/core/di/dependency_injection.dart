ac0998047c93e2a83ef974a6b3a1b0ba2977e777
MohamedElhosaraty <mohamedelhosaraty05@gmail.com>
2026-04-27 03:13:33 +0300

    remove old home screen

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -1,5 +1,4 @@
 import 'package:fitness/core/helpers/firestore_service.dart';
-import 'package:fitness/feature/home/ui/cubit/add_exercises/add_exercises_cubit.dart';
 import 'package:get_it/get_it.dart';
 
 import '../../feature/auth/data/repo/auth_repo_impl.dart';
@@ -7,11 +6,6 @@ import '../../feature/auth/data/service/firebase_auth_service.dart';
 import '../../feature/auth/domain/repos/auth_repo.dart';
 import '../../feature/auth/ui/cubits/sign_in_cubit/signin_cubit.dart';
 import '../../feature/auth/ui/cubits/signup_cubit/signup_cubit.dart';
-import '../../feature/home/data/repo/workout_repo_impl.dart';
-import '../../feature/home/data/service/firebase_workout_service.dart';
-import '../../feature/home/domain/repos/workout_repo.dart';
-import '../../feature/home/ui/cubit/get_all_day_exercises/get__all_day_exercises_cubit.dart';
-import '../../feature/home/ui/cubit/get_exercises/get_exercises_cubit.dart';
 import '../../feature/onboarding/data/repo/onboarding_repo_impl.dart';
 import '../../feature/onboarding/data/service/firestore_onboarding_service.dart';
 import '../../feature/onboarding/domain/repo/onboarding_repo.dart';
@@ -35,18 +29,6 @@ Future<void> setupGetIt() async {
     ),
   );
 
-  // 💪 Workout
-  getIt.registerSingleton<FirebaseWorkoutService>(
-    FirebaseWorkoutService(getIt<FirestoreService>()),
-  );
-
-  getIt.registerSingleton<WorkoutRepo>(
-    WorkoutRepoImpl(
-      getIt<FirebaseWorkoutService>(),
-      getIt<RemoteConfigService>(),
-    ),
-  );
-
   // 🎯 Onboarding
   getIt.registerSingleton<FirestoreOnboardingService>(
     FirestoreOnboardingService(firestoreService: getIt<FirestoreService>()),
@@ -62,9 +44,6 @@ Future<void> setupGetIt() async {
   // 🧠 Cubits
   getIt.registerFactory(() => SignupCubit(getIt<AuthRepo>()));
   getIt.registerFactory(() => SignInCubit(getIt<AuthRepo>()));
-  getIt.registerFactory(() => GetExercisesCubit(getIt<WorkoutRepo>(),));
-  getIt.registerFactory(() => AddExercisesCubit(getIt<WorkoutRepo>()));
-  getIt.registerFactory(() => GetAllDayExercisesCubit(getIt<WorkoutRepo>()));
   getIt.registerFactory(() => OnboardingCubit(onboardingRepo: getIt<OnboardingRepo>()));
   getIt.registerFactory(() => LocalizationCubit());
 
--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,20 +1,10 @@
 import 'package:fitness/core/routing/routes.dart';
 import 'package:fitness/feature/auth/ui/page/signin_view.dart';
 import 'package:fitness/feature/auth/ui/page/signup_view.dart';
-import 'package:fitness/feature/home/ui/page/choose_training_split_view.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-import '../../feature/home/data/model/day_schedule_model.dart';
-import '../../feature/home/domain/entity/training_split_type.dart';
-import '../../feature/home/ui/cubit/add_exercises/add_exercises_cubit.dart';
-import '../../feature/home/ui/cubit/get_exercises/get_exercises_cubit.dart';
-import '../../feature/home/ui/page/select_exercises_view.dart';
-import '../../feature/home/ui/page/weekly_schedule_screen.dart';
 import '../../feature/main_screen/ui/main_screen.dart';
 import '../../feature/onboarding/ui/page/onboarding_screen.dart';
 import '../../feature/splash/splash_screen.dart';
-import '../di/dependency_injection.dart';
 
 class AppRouter {
   static Route? generateRoute(RouteSettings settings) {
@@ -34,32 +24,7 @@ class AppRouter {
       case Routes.mainScreen:
         return MaterialPageRoute(builder: (_) => const MainScreen());
 
-      case Routes.chooseTrainingSplitView:
-        return MaterialPageRoute(builder: (_) => const ChooseTrainingSplitView());
 
-      case Routes.weeklyScheduleScreen:
-        final splitType = settings.arguments as TrainingSplitType;
-        final exercisesCubit = getIt<GetExercisesCubit>();
-        return MaterialPageRoute(
-          builder: (_) => BlocProvider(
-            create: (context) => exercisesCubit,
-            child: WeeklyScheduleScreen(splitType: splitType),
-          ),
-        );
-
-      case Routes.selectExercisesView:
-        final args = settings.arguments as Map<String, dynamic>;
-        final cubit = args['cubit'] as GetExercisesCubit;
-        final day = args['day'] as DayScheduleModel;
-        return MaterialPageRoute(
-          builder: (_) => MultiBlocProvider(
-            providers: [
-              BlocProvider.value(value: cubit),
-              BlocProvider(create: (_) => getIt<AddExercisesCubit>()),
-            ],
-            child: SelectExercisesView(daySchedule: day),
-          ),
-        );
     }
     return null;
   }
